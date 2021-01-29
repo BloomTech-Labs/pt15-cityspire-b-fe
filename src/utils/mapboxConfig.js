34ec68b1eac1b90a5eecc990f7248dc27957374e
Allison Usher <allyjay317@gmail.com>
2021-01-28 22:22:23 -0800

    added mapboxConfig

---
+++ src/utils/mapboxConfig.js
@@ -0,0 +1,9 @@
+const mapboxConfig = {
+  token: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+  lng: 5,
+  lat: 34,
+  zoom: 2,
+  style: 'mapbox://styles/mapbox/streets-v11',
+};
+
+export { mapboxConfig };
