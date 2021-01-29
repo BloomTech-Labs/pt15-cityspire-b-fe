5c1731e79b3d3c887ade097edc3eee51c1d1d1d6
Allison Usher <allyjay317@gmail.com>
2021-01-28 22:23:02 -0800

    added geocoder and map to landingpage

--- src/components/pages/Landing/RenderLandingPage.js
+++ src/components/pages/Landing/RenderLandingPage.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-
-
+import GeoCoderContainer from '../Home/Geocoder';
+import MapContainer from '../Home/Map';
 
 function RenderLandingPage(props) {
-
   const history = useHistory();
 
-  const loginHandler = () =>{
+  const [map, setMap] = useState(undefined);
+  const [geocoder, setGeocoder] = useState(undefined);
+
+  useEffect(() => {
+    if (map && geocoder) {
+      map.addControl(geocoder);
+    }
+  }, [map, geocoder]);
+
+  const loginHandler = () => {
     history.push('/login');
   };
 
-
   return (
     <div>
-      <h1>Welcome to Labs Basic SPA</h1>
-      <div>
-        <p>
-          This is an example of how we'd like for you to approach page/routable
-          components.
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
-        </p>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '50%',
+          left: '50%',
+          marginLeft: '-25%',
+          zIndex: 2,
+          backgroundColor: 'white',
+          textAlign: 'center',
+          marginBottom: '10%',
+        }}
+      >
+        <h1>Welcome to Labs Basic SPA</h1>
+        <div>
+          <p>
+            This is an example of how we'd like for you to approach
+            page/routable components.
+          </p>
+          <p>
+            <Link to="/example-list">Example List of Items</Link>
+          </p>
+        </div>
+        <button onClick={loginHandler}>Login</button>
       </div>
-      <button onClick={loginHandler}>Login</button>
+      <div>
+        <GeoCoderContainer setGeocoder={setGeocoder} />
+        <MapContainer setMap={setMap} />
+      </div>
     </div>
   );
 }
