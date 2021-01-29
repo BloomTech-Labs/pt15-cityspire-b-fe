0976e879768a72684dd00c56e0f56afc2b200c90
Allison Usher <allyjay317@gmail.com>
2021-01-28 23:41:31 -0800

    reverse geocoding on move, retrieving data

--- src/components/pages/Landing/RenderLandingPage.js
+++ src/components/pages/Landing/RenderLandingPage.js
@@ -1,17 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { mapboxConfig } from '../../../utils/mapboxConfig';
 import GeoCoderContainer from '../Home/Geocoder';
 import MapContainer from '../Home/Map';
+
+const mapboxClient = require('@mapbox/mapbox-sdk');
+const mapboxGeocode = require('@mapbox/mapbox-sdk/services/geocoding');
+const baseClient = mapboxClient({ accessToken: mapboxConfig.token });
+const geocodeService = mapboxGeocode(baseClient);
 
 function RenderLandingPage(props) {
   const history = useHistory();
 
   const [map, setMap] = useState(undefined);
   const [geocoder, setGeocoder] = useState(undefined);
+  const [coords, setCoords] = useState({ lat: 0, lng: 0 });
 
   useEffect(() => {
     if (map && geocoder) {
       map.addControl(geocoder);
+      map.on('move', () => {
+        const lat = map.getCenter().lat.toFixed(4);
+        const lng = map.getCenter().lng.toFixed(4);
+
+        geocodeService
+          .reverseGeocode({
+            query: [parseFloat(lng), parseFloat(lat)],
+            types: ['place', 'postcode'],
+          })
+          .send()
+          .then(res => {
+            const features = res.body.features;
+            if (features.length > 0) {
+              console.log(features);
+              const [city, state, country] = features[1].place_name.split(',');
+              const zipcode = parseInt(features[0].text);
+              setCoords({ ...coords, city, state, zipcode });
+            }
+            // if (res.body.features.length > 0) {
+            //   const
+            //
+            // }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+        setCoords({
+          lat,
+          lng,
+        });
+      });
     }
   }, [map, geocoder]);
 
