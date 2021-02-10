63d3f4e76253f2dce37b3ab2180b13379484c798
Allison Usher <allyjay317@gmail.com>
2021-02-09 18:22:32 -0800

    adjusted setupTests to account for missing secret

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,8 +5,25 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-prop-type-error';
 import 'jest-canvas-mock';
+import mapboxClient from '@mapbox/mapbox-sdk';
+const mapboxGeocode = require('@mapbox/mapbox-sdk/services/geocoding');
 
 jest.mock('mapbox-gl');
+jest.mock('@mapbox/mapbox-sdk');
+mapboxClient.mockResolvedValue({ mockClient: true });
+jest.mock('@mapbox/mapbox-sdk/services/geocoding');
+mapboxGeocode.mockResolvedValue({
+  mockClient: true,
+  reverseGeoCode: obj => {
+    return new Promise((resolve, reject) => {
+      resolve({ features: [{ city: 'someplace', state: 'wa' }] });
+      reject({
+        error: 'this is an error',
+      });
+    });
+  },
+});
+
 window.URL.createObjectURL = function() {};
 window.matchMedia = function() {
   return { addListener: jest.fn(), removeListener: jest.fn() };
