767888fc9de6e4471aecedb58ff8513d0f06fb61
Allison Usher <allyjay317@gmail.com>
2021-02-12 19:13:18 -0800

    defined map actions and action creators

---
+++ src/state/actions/mapActions.js
@@ -0,0 +1,8 @@
+export const REVERSE_GEOCODE = 'REVERSE_GEOCODE';
+export const INITIALIZE_MAP = 'INITIALIZE_MAP';
+export const START_MOVE = 'START_MOVE';
+export const STOP_MOVE = 'STOP_MOVE';
+
+export const reverseGeocode = ([lat, lng]) => dispatch => {};
+
+export const initializeMap = (mapboxgl, container) => dispatch => {};
