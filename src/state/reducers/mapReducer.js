5e3ae95a74fffdbb4acaf8a217d7a6f27be0ae2e
Allison Usher <allyjay317@gmail.com>
2021-02-12 19:14:44 -0800

    defined state changes for actions

--- src/state/reducers/mapReducer.js
+++ src/state/reducers/mapReducer.js
@@ -1,3 +1,5 @@
+import { types } from '../actions';
+
 const init = {
   coords: {
     city: '',
@@ -8,10 +10,37 @@ const init = {
   },
   map: undefined,
   geocoder: undefined,
+  isMoving: true,
+  movementCoords: [[]],
 };
 
 export default function mapReducer(state = init, action) {
   switch (action.type) {
+    case types.INITIALIZE_MAP:
+      return {
+        ...state,
+        map: action.payload,
+      };
+    case types.REVERSE_GEOCODE:
+      return {
+        ...state,
+        coords: { ...init.coords, ...action.payload },
+      };
+    case types.START_MOVE:
+      return {
+        ...state,
+        isMoving: true,
+        movementCoords: [...state.movementCoords, [...action.payload]],
+      };
+    case types.STOP_MOVE:
+      return {
+        ...state,
+        isMoving: state.movementCoords.length > 2,
+        movementCoords: state.movementCoords.slice(
+          1,
+          state.movementCoords.length
+        ),
+      };
     default:
       return state;
   }
